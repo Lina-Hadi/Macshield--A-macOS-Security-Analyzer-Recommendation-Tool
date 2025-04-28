fdfca1dfe0e7a9bf87da5eb737c5cf243a95d522
whoamif <adjissifatimaamina@gmail.com>
2025-04-28 22:07:07 +0100

    everything is fixeddddddd ,meryumii

--- app/sip_analyzer.py
+++ app/sip_analyzer.py
@@ -36,4 +36,5 @@ def check_sip_status():
 
 if __name__ == "__main__":
     result = check_sip_status()
-    
+    # Ajout de cette ligne pour afficher le résultat
+    print(json.dumps(result, indent=4))
